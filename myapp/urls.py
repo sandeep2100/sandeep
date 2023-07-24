6e8c54a46bc30c1b6e7f2d7b776312eb264d39ed
sandeep2100 <sandeep@godbms.com>
2023-07-24 17:57:07 +0530

    commit

--- myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import OTPView, OTPVerificationView
-
-urlpatterns = [
-       path('otp/', OTPView.as_view(), name='otp'),
-       path('otp-verification/', OTPVerificationView.as_view(), name='otp_verification'),
-
-]
